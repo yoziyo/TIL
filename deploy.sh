75fbb420af86168e91ca42c90a7974793b8385bb
hjjang <hjjang0207@yellostory.co.kr>
2019-11-05 15:02:30 +0900

    Test

--- deploy.sh
+++ deploy.sh
@@ -3,16 +3,19 @@
 # abort on errors
 set -e
 
-git checkout gh-pages
-
 # build
 npm run build
 
+git add docs/.vuepress/dist -f
+git commit -m 'vuepress commit'
+git push origin `git subtree split --prefix docs/.vuepress/dist master`:gh-pages --force
+
+
 # navigate into the build output directory
-cd docs/.vuepress/dist
+# cd docs/.vuepress/dist
 
-git add -A
-git commit -m 'vuepress deploy'
-git push -f origin gh-pages
+# git add -A
+# git commit -m 'vuepress deploy'
+# git push -f origin gh-pages
 
-cd -
+# cd -
