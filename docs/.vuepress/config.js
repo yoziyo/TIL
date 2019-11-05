b9c616d28e844b1aa9fa6e130a09cdbc2e70d746
hjjang <hjjang0207@yellostory.co.kr>
2019-11-05 16:58:32 +0900

    edit package.json

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  title: `Yozi Today I Learned`,
+  description: `작업 및 공부나 서칭 중 발견한걸 쓱싹쓱싹 합니다.`,
+  base: "/",
+  dest: 'build',
+  head: [],
+  themeConfig: {
+    sidebar: [
+      {
+        title: '0. about'
+      }
+    ],
+    nav: [{
+        text: 'GitHub',
+        link: 'https://github.com/yoziyo/'
+      }
+    ]
+  },
+}
