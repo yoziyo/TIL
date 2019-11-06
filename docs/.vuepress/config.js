d8efd5c9aff6e89cfa64d7c42bcbb9eacc14f4bd
yozi <j16412000kr@gmail.com>
2019-11-06 22:25:12 +0900

    ga추가

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -6,7 +6,16 @@ module.exports = {
     description: `작업 및 공부나 서칭 중 발견한걸 쓱싹쓱싹 합니다.`,
     base: "/",
     dest: 'build',
-    head: [],
+    head: [
+        ['link', {rel: 'icon', href: `logo-144.png`}],
+        ['meta', {name: 'theme-color', content: '#5b19de'}],
+        ['meta', {name: 'apple-mobile-web-app-capable', content: 'yes'}],
+        ['meta', {name: 'apple-mobile-web-app-status-bar-style', content: 'black'}],
+        ['link', {rel: 'apple-touch-icon', href: `logo-144.png`}],
+        ['link', {rel: 'mask-icon', href: 'logo-144.png', color: '#5b19de'}],
+        ['meta', {name: 'msapplication-TileImage', content: 'logo-144.png'}],
+        ['meta', {name: 'msapplication-TileColor', content: '#000000'}]
+    ],
     themeConfig: {
         sidebar: [
             {
@@ -33,4 +42,18 @@ module.exports = {
             link: 'https://github.com/yoziyo/'
         }]
     },
+    plugins: [
+        ['@vuepress/google-analytics', {
+            'ga': 'UA-151651051-1'
+        }],
+        ['@vuepress/pwa', {
+            serviceWorker: true,
+            updatePopup: true
+        }],
+        ['@limdongjin/vuepress-plugin-simple-seo', {
+            default_image: 'logo.png',
+            root_url: 'https://blog.yozi.kr/',
+            default_site_name: 'YOZI TIL'
+        }]
+    ],
 }
